c2e32152b1585a20a13f0703a08ded56599a932a
TsLuis <lfss98@hotmail.com>
2017-06-10 17:52:36 +0100

    Adicionei os GameDesigners burros.

--- src/frontend/Menu.java
+++ src/frontend/Menu.java
@@ -19,7 +19,7 @@ public class Menu extends JFrame {
     
     private JFrame anterior;
     private List<Festival> fest;
-
+    
     /**
      * Creates new form Menu
      */
@@ -95,8 +95,18 @@ public class Menu extends JFrame {
         });
 
         jButton2.setText("Novo Colaborador");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Novo Game Designer");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Novo Patrocinador");
 
@@ -243,6 +253,19 @@ public class Menu extends JFrame {
         this.setVisible(false);
         novoFestival.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        GameDesigner gameDesigner = new GameDesigner(this);
+        gameDesigner.setVisible(true);
+        gameDesigner.setLocationRelativeTo(null);
+        
+        this.setVisible(false);
+        gameDesigner.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
